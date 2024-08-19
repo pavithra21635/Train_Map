0ce017d9869d2b630933e64c5765d793b566e0de
pavithra21635 <pavithra21635@gmail.com>
2024-08-19 23:44:22 +0530

    Creating sri lanka map

---
+++ src/map/train/index.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon from '../train/train-pin.png';
+import L from 'leaflet';
+
+const center = [7.688843, 80.665844];
+
+
+
+  
+ 
+
+ 
