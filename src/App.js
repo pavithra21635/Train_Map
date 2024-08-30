4a390f24a27afe7c57a6ab2e9fbd1a22ac1a13a4
pavithra21635 <pavithra21635@gmail.com>
2024-08-31 02:21:37 +0530

    Revert "Adding user authentication before the map loads"
    This reverts commit 5ea3ce1cd0508264380fb6ac0c1b2d1f4be0af88.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// src/App.js
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MapIndex from './map/train/MapIndex';
-import Login from './map/train/Login';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import MapIndex from './map/train';
+
+
 
 const App = () => {
-  const isAuthenticated = !!localStorage.getItem('token'); // Check for token
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {isAuthenticated ? (
-          <Route path="/" element={<MapIndex />} />
-        ) : (
-          <Route path="/login2" element={<Login />} />
-        )}
+     <Routes>
+        <Route path="/" element={<MapIndex />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
+
