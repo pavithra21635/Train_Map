860601400c101464cfc7f12fe1a9bea9d03eecf7
pavithra21635 <pavithra21635@gmail.com>
2024-08-31 11:20:55 +0530

    Fixing navigating issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         {isAuthenticated ? (
           <Route path="/" element={<MapIndex />} />
         ) : (
-          <Route path="/login2" element={<Login />} />
+          <Route path="/" element={<Login />} />
         )}
       </Routes>
     </BrowserRouter>
